3d8a2a913cf62e3268a053cd7dd1a16e31aa3b1f
jiwon-u <jayde0yu@gmail.com>
2025-06-11 16:57:39 +0900

    implement product table

--- filterable-product-table/src/App.jsx
+++ filterable-product-table/src/App.jsx
@@ -1,13 +1,13 @@
 
 function ProductRow({ product }) {
   const colorStyle = {
-    color: product.stocked ? 'black' : 'red';
-  }
+    color: product.stocked ? 'black' : 'red'
+  };
 
   return (
     <>
       <tr>
-        <td style="{colorStyle}">{product.name}</td>
+        <td style={colorStyle}>{product.name}</td>
         <td>{product.price}</td>
       </tr>
     </>
@@ -17,13 +17,46 @@ function ProductRow({ product }) {
 function ProductCategoryRow({ category }) {
   return(
     <>
+      <tr>
+        <th colSpan="2">{category}</th>
+      </tr>
     </>
   );
 }
 
 function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}/>
+        );
+    } 
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
   return(
     <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     </>
   );
 }
